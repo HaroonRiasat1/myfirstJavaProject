c82a9932b3f602df61163c9e92574aa5f7c949e8
HaroonRiasat1 <haroonriasata@gmail.com>
2021-09-13 21:38:40 +0500

    test1

--- hello/src/hello/hello.java
+++ hello/src/hello/hello.java
@@ -1,8 +1,23 @@
 package hello;
+import java.util.Scanner;
+
 
 public class hello {
+	
+	
+	
+	
+	
+	
 	public static void main(String[] arg) {
+
+		Scanner myObj=new Scanner(System.in);
 	System.out.println("Hello World");	
+	
+	String input1=myObj.nextLine();
+	
+	
+	
 	}
 
 }
